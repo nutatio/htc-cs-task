ee4de6288098da6f7520650ca1aeb85c43dea758
nutatio <dar489@gmail.com>
2021-07-13 22:16:40 +0400

    add image hover

---
+++ sripts.js
@@ -0,0 +1,5 @@
+const filmBtn = document.getElementById('film-tab-btn');
+const channelBtn = document.getElementById('channel-tab-btn');
+const authBtn = document.getElementById('auth-btn');
+const popupBtn = document.getElementById('popup-btn');
+
