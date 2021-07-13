93964d7a554d75cb334a26c9f74506b32449ad02
nutatio <dar489@gmail.com>
2021-07-14 00:01:24 +0400

    popup fixed, username displayed, enter\exit button

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const channelBtn = document.getElementById('channel-tab-btn');
 const authBtn = document.getElementById('auth-btn');
 const popupBtn = document.getElementById('popup-btn');
 const films = document.getElementById('films-content');
-const channels = document.getElementById('channels-content')
+const channels = document.getElementById('channels-content');
+const popup = document.getElementById('popup-container');
+const popupLogin = document.getElementById('login');
+const auth = document.getElementById('auth');
+const authExitBtn = document.getElementById('auth-btn-exit');
 
 const active = 'active';
+
+console.log(authExitBtn);
+
 filmBtn.addEventListener('click', () =>{
   if(filmBtn.classList.contains(active) === false){
     filmBtn.classList.toggle(active);
@@ -25,3 +32,24 @@ channelBtn.addEventListener('click', () =>{
   }
   channels.style.display='flex';
 }) 
+
+authBtn.addEventListener('click', () =>{
+  popup.style.display = 'flex';
+})
+
+popupBtn.addEventListener('click', () =>{
+  popup.style.display = 'none';
+  authBtn.setAttribute('hidden','enabled');
+  authExitBtn.removeAttribute('hidden');
+  let name = popupLogin.value;
+  auth.innerHTML = `
+    <span>${name}</span>
+  `;
+
+})
+
+authExitBtn.addEventListener('click', () =>{
+  authExitBtn.setAttribute('hidden','enabled');
+  authBtn.removeAttribute('hidden');
+  auth.innerHTML = ``;
+})
